1299ceb7a4e473099c4b2fd9e25036bc9009317f
Hayata Suenaga <hayata@expensify.com>
2023-10-14 21:12:14 +0900

    feat: init prisma

---
+++ script.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+async function main() {
+   console.dir(await getAllUsers(), {depth: null});
+}
+
+async function getAllUsers() {
+    const users = await prisma.user.findMany(
+        {
+            include: {
+                posts: true,
+            }
+        }
+    );
+    return users;
+}
