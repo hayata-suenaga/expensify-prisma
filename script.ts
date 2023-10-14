8f9e1ddd14477bc73ab3fd691bb499b72a603a1d
Hayata Suenaga <hayata@expensify.com>
2023-10-14 22:22:15 +0900

    feat: add basic functions

--- script.ts
+++ script.ts
@@ -1,6 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+const mockUsers: Prisma.UserCreateInput[] = [
+  {
+    firstName: "David",
+    lastName: "Barrett",
+    email: "dbarrett@expensify.com ",
+  },
+  {
+    firstName: "Hayata",
+    lastName: "Suenaga",
+    email: "hsuenaga.expensify.com",
+  },
+];
 
 main()
   .then(async () => {
@@ -13,10 +26,22 @@ main()
   });
 
 async function main() {
-  console.dir(await getAllUsers(), { depth: null });
+  const newUser = await createUsers(mockUsers);
+  const users = await getAllUsers();
+  console.log(users);
+}
+
+async function createUsers(users: Prisma.UserCreateInput[]) {
+  for (const user of users) {
+    await prisma.user.create({ data: user });
+  }
 }
 
 async function getAllUsers() {
   const users = await prisma.user.findMany();
   return users;
+}
+
+async function getFeed(userId: string) {
+  const user = await prisma.user.findUnique({ where: { id: userId } });
 }
